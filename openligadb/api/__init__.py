6a893f74254e242034672217dd1c69f120b8809e
Michael Hallmann <halli@gmx.de>
2024-12-07 17:20:05 +0100

    Update __init__.py

--- openligadb/api/__init__.py
+++ openligadb/api/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.matchdata_api import MatchdataApi
+from openligadb.api.matchdata_api import MatchdataApi
