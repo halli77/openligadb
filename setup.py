ed8e53e32cd4978f504573a82982fadc2ab69783
Michael Hallmann <halli@gmx.de>
2024-12-08 10:47:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "openligadb_python_client"
-VERSION = "0.0.7"
+VERSION = "1.0.7"
 # To install the library, run the following
 #
 # python setup.py install
